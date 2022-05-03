2a2466d07c6880d98fb90f7e91e337291b6e225b
Gobila <12902689+gobila@users.noreply.github.com>
2022-05-03 14:55:24 -0300

    form conta

--- src/components/FormWallet/index.js
+++ src/components/FormWallet/index.js
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Form, Col, Row, Button,
 } from 'react-bootstrap';
 
+const centralize = {
+  display: 'flex',
+  margin: 'auto',
+};
+
 export default function FormWallet() {
-  const centralize = {
-    display: 'flex',
-    margin: 'auto',
+  const [conta, setConta] = useState({
+    moeda: '',
+    valor: '',
+    data: '',
+    moedaBase: 'BRL',
+    valorFinal: 0,
+  });
+
+  const onChangeValue = (e) => {
+    const { value } = e.target;
+    const { name } = e.target;
+
+    setConta((currentValues) => (
+      {
+        ...currentValues,
+        [name]: value,
+      }
+    ));
   };
+  useEffect(() => {
+    const taxa = conta.valor * 4;
+    setConta((currentValues) => (
+      {
+        ...currentValues,
+        valorFinal: taxa,
+      }
+    ));
+  }, [conta.valor]);
   return (
     <Form className="col-10">
       <Row className="align-items-center">
         <Form.Group as={Col} sm={4} className="mb-3" controlId="formGridCoin">
-          <Form.Label>
-            Moeda
-          </Form.Label>
-          <Form.Control placeholder="moeda" />
+          <Form.Label> Moeda </Form.Label>
+          <Form.Control name="moeda" onChange={onChangeValue} />
         </Form.Group>
+
         <Form.Group as={Col} sm={4} className="mb-3" controlId="formGridValue">
-          <Form.Label>
-            Valor
-          </Form.Label>
-          <Form.Control type="number" placeholder="Valor" />
+          <Form.Label> Valor </Form.Label>
+          <Form.Control type="number" name="valor" onChange={onChangeValue} />
         </Form.Group>
+
         <Form.Group as={Col} sm={4} className="mb-3" controlId="formGridDate">
-          <Form.Label>
-            Data
-          </Form.Label>
-          <Form.Control type="date" placeholder="data" />
+          <Form.Label> Data </Form.Label>
+          <Form.Control type="date" name="data" onChange={onChangeValue} />
         </Form.Group>
       </Row>
 
@@ -38,7 +63,7 @@ export default function FormWallet() {
               Moeda Base
             </Form.Label>
             <Col sm={6}>
-              <Form.Control placeholder="Moeda Base" disabled />
+              <Form.Control placeholder="Moeda Base" value={conta.moedaBase} disabled />
             </Col>
           </Row>
         </Form.Group>
@@ -49,7 +74,7 @@ export default function FormWallet() {
               Valor Final
             </Form.Label>
             <Col sm={6}>
-              <Form.Control placeholder="Valor Final" disabled />
+              <Form.Control placeholder="Valor Final" value={conta.valorFinal} disabled />
             </Col>
           </Row>
         </Form.Group>
