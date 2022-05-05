5adf9e0c45a6c5787e7b044aaeb3ef5b1eff0034
Gobila <12902689+gobila@users.noreply.github.com>
2022-05-05 15:56:42 -0300

    bio

--- pages/profile.js
+++ pages/profile.js
@@ -1,12 +1,40 @@
 import React, { useContext } from 'react';
+import {
+  Card, Col, Container, Row,
+} from 'react-bootstrap';
+import Menu from '../src/components/Menu';
 import { UserContext } from '../src/Context';
 
 export default function FormLogin() {
-  const user = useContext(UserContext);
+  const { user } = useContext(UserContext);
   console.log(user);
   return (
     <div>
-      <p>{user.login}</p>
+      <Menu />
+      <Container className="mt-10" sm={10}>
+        <Row className="mt-10">
+          <Col sm={5}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={user.avatar_url} />
+              <Card.Body>
+                <Card.Title>{user.login}</Card.Title>
+                <Card.Text>
+                  {user.bio}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={5}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Text>
+                  {user.bio}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
